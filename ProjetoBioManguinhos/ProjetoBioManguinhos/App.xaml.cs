c48128126e1c73bfccd08958a8b8e86d8173835d
Apolock33 <carlosammgomes@gmail.com>
2024-02-07 14:33:52 -0300

    Projeto Criado

---
+++ ProjetoBioManguinhos/ProjetoBioManguinhos/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ProjetoBioManguinhos.Services;
+using ProjetoBioManguinhos.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjetoBioManguinhos
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ProjetoBioManguinhos/ProjetoBioManguinhos/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ProjetoBioManguinhos.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ProjetoBioManguinhos.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
